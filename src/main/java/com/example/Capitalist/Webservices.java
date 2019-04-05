2bbf75fa54e544b9d9facd28a6efa0a47817520f
supermagicmike <mickaelcommerot1@gmail.com>
2019-04-05 10:44:16 +0200

    before soutenance

--- src/main/java/com/example/Capitalist/Webservices.java
+++ src/main/java/com/example/Capitalist/Webservices.java
@@ -9,9 +9,15 @@ package com.example.Capitalist;
  *
  * @author Mick
  */
+import com.google.gson.Gson;
+import generated.PallierType;
+import generated.ProductType;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBException;
@@ -28,8 +34,28 @@ public class Webservices {
     @GET
     @Path("world")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getWorld() throws JAXBException {
-        return Response.ok(services.readWorldFromXml()).build();
+    public Response getWorld(@Context HttpServletRequest request) throws JAXBException {
+        String username= request.getHeader("X-user");
+        System.out.println("username: "+username);
+        return Response.ok(services.readWorldFromXml(username)).build();
     }
+    
+    @PUT
+    @Path("product")
+    public Response Product(@Context HttpServletRequest request,String body) throws JAXBException {
+        String username= request.getHeader("X-user");
+        ProductType product = new Gson().fromJson(body, ProductType.class); 
+        return Response.ok(services.updateProduct(username,product)).build();
+    }
+    
+    @PUT
+    @Path("manager")
+    public Response Manager(@Context HttpServletRequest request, String body) throws JAXBException {
+        String username= request.getHeader("X-user");
+        PallierType manager = new Gson().fromJson(body, PallierType.class); 
+        
+        return Response.ok(services.updateManager(username,manager)).build();
+    }
+    
 
 }
